234e75feea205ef8e0ef24f9c3330efb31ac6614
Lucas Thompson <dev@lucas.im>
2017-05-04 21:30:30 +0100

    Try sample response. Should validate.

--- index.js
+++ index.js
@@ -4,16 +4,15 @@
  * @param {!Object} req Cloud Function request context.
  * @param {!Object} res Cloud Function response context.
  */
-exports.helloWorld = function helloWorld(req, res) {
-  // Example input: {"message": "Hello!"}
-  if (req.body.message === undefined) {
-    // This is an error case, as "message" is required.
-    res.status(400).send('No message defined!');
-  } else {
-    // Everything is okay.
-    console.log(req.body.message);
-    res.status(200).send('Success: ' + req.body.message);
-  }
+exports.helloWorld = (req, res) => {
+  if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === 'dooley_party_bot') {
+      console.log("Validating webhook");
+      res.status(200).send(req.query['hub.challenge']);
+    } else {
+      console.error("Failed validation. Make sure the validation tokens match.");
+      res.sendStatus(403);          
+    }  
 };
 
 
