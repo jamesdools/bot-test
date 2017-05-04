77187ad05050da298303e67b00c29c1f0438182d
Lucas Thompson <dev@lucas.im>
2017-05-04 22:06:02 +0100

    Primitive router in place and a bunch of copy paste up to step 6.

--- index.js
+++ index.js
@@ -4,15 +4,128 @@
  * @param {!Object} req Cloud Function request context.
  * @param {!Object} res Cloud Function response context.
  */
-exports.helloWorld = (req, res) => {
-  if (req.query['hub.mode'] === 'subscribe' &&
-        req.query['hub.verify_token'] === 'dooley_party_bot') {
-      console.log("Validating webhook");
-      res.status(200).send(req.query['hub.challenge']);
-    } else {
-      console.error("Failed validation. Make sure the validation tokens match.");
-      res.sendStatus(403);          
-    }  
+exports.primitiveRouter = (req, res) => {
+  switch(req.method) {
+    case 'GET':
+      handleGet(req, res);
+      break;
+    case 'POST':
+      handlePost(req, res);
+      break;
+    default:
+      res.status(500).send({error: '👎'});
+  }
 };
 
+const SHOULD_NOT_BE_IN_VCS = 'EAAGQI7zUsMkBACWpMiDAsoS5KoAQ8v4o6nO8A922eYhoN1KnrcZCYrvOOhNcDXCU3EO3P2LHBcwqNyEPoJFS7hJyAESPZC6E1fUKLEQQrxS7y3T9cpZAgKhpAPDXCzfsLmhpGPiIOvd8OC8lGsQZCdQaTUPJSx3JguXHIVJJmQZDZD';
 
+const handleGet = (req, res) => { 
+  const isSubscriptionMode = req.query['hub.mode'] === 'subscribe';
+  const hasValidToken = req.query['hub.verify_token'] === 'dooley_party_bot');
+  if (isSubscriptionMode && hasValidToken) {
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    res.status(403).send({error: '🚫'});    
+  }
+};
+
+const handlePost = (req, res) = {
+  const data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach((entry) => {
+      const pageID = entry.id;
+      const timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+};
+
+/// mostly copy paste from step 6. in Quick Start guide
+
+function receivedMessage(event) {
+  const senderID = event.sender.id;
+  const recipientID = event.recipient.id;
+  const timeOfMessage = event.timestamp;
+  const message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  const messageId = message.mid;
+
+  const messageText = message.text;
+  const messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+function sendGenericMessage(recipientId, messageText) {
+  // To be expanded in later sections
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: SHOULD_NOT_BE_IN_VCS },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      const recipientId = body.recipient_id;
+      const messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
+}
